5e738363e87bcd897d18abdff72af15055e2329f
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 20:23:21 +0300

    Update ExampleInstrumentedTest.java

--- dialog/src/androidTest/java/ru/mirea/klishenkovai/dialog/ExampleInstrumentedTest.java
+++ dialog/src/androidTest/java/ru/mirea/klishenkovai/dialog/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ru.mirea.klishenkovaai.dialog;
+package ru.mirea.klishenkovai.dialog;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.klishenkovaai.dialog", appContext.getPackageName());
+        assertEquals("ru.mirea.klishenkovai.dialog", appContext.getPackageName());
     }
 }
