4463e20e637b949f01492127e15aa00de9ba069a
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 20:32:42 +0300

    Update build.gradle.kts

--- intentfilter/build.gradle.kts
+++ intentfilter/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "ru.mirea.klishenkovaai.intentfilter"
+    namespace = "ru.mirea.klishenkovai.intentfilter"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "ru.mirea.klishenkovaai.intentfilter"
+        applicationId = "ru.mirea.klishenkovai.intentfilter"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
