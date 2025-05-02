46a5de4b4b70940c9dbd582654ead017bc2286a6
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 19:58:05 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "ru.mirea.klishenkovaai.lesson2"
+    namespace = "ru.mirea.klishenkovai.lesson2"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "ru.mirea.klishenkovaai.lesson2"
+        applicationId = "ru.mirea.klishenkovai.lesson2"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
