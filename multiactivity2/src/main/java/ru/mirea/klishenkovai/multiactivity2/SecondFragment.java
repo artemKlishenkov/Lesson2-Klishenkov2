7e68db96d41120a54a5501c19c867bc667ca6073
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 20:48:51 +0300

    Update SecondFragment.java

--- multiactivity2/src/main/java/ru/mirea/klishenkovai/multiactivity2/SecondFragment.java
+++ multiactivity2/src/main/java/ru/mirea/klishenkovai/multiactivity2/SecondFragment.java
@@ -1,4 +1,4 @@
-package ru.mirea.klishenkovaai.multiactivity2;
+package ru.mirea.klishenkovai.multiactivity2;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import ru.mirea.klishenkovaai.multiactivity2.databinding.FragmentSecondBinding;
+import ru.mirea.klishenkovai.multiactivity2.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
 
