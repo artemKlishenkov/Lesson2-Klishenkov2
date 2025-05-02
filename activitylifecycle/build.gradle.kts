86258f24971a15cb7df596a47e376d7d7058e239
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 19:36:21 +0300

    Update build.gradle.kts

--- activitylifecycle/build.gradle.kts
+++ activitylifecycle/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "ru.mirea.klishenkovaai.activitylifecycle"
+    namespace = "ru.mirea.klishenkovai.activitylifecycle"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "ru.mirea.klishenkovaai.activitylifecycle"
+        applicationId = "ru.mirea.klishenkovai.activitylifecycle"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
