e490d3df53d5566ab8fa659378cb4379926e90bd
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 20:44:48 +0300

    Update ExampleInstrumentedTest.java

--- multiactivity2/src/androidTest/java/ru/mirea/klishenkovai/multiactivity2/ExampleInstrumentedTest.java
+++ multiactivity2/src/androidTest/java/ru/mirea/klishenkovai/multiactivity2/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ru.mirea.klishenkovaai.multiactivity2;
+package ru.mirea.klishenkovai.multiactivity2;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.klishenkovaai.multiactivity2", appContext.getPackageName());
+        assertEquals("ru.mirea.klishenkovai.multiactivity2", appContext.getPackageName());
     }
 }
