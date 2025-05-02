b75cdf387c4d1009f693549f5eb4b75d134407a0
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 19:40:38 +0300

    Update ExampleInstrumentedTest.java

--- activitylifecycle/src/androidTest/java/ru/mirea/klishenkovai/activitylifecycle/ExampleInstrumentedTest.java
+++ activitylifecycle/src/androidTest/java/ru/mirea/klishenkovai/activitylifecycle/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ru.mirea.klishenkovaai.activitylifecycle;
+package ru.mirea.klishenkovai.activitylifecycle;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.klishenkovaai.activitylifecycle", appContext.getPackageName());
+        assertEquals("ru.mirea.klishenkovai.activitylifecycle", appContext.getPackageName());
     }
 }
