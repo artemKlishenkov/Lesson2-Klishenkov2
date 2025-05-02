39a8009e26823d8b78684de5e263d07e214d05f6
Клишенков Артём Игоревич БСБО-06-23 <73350652+artemKlishenkov@users.noreply.github.com>
2025-05-02 21:06:54 +0300

    Update ExampleInstrumentedTest.java

--- notificationapp/src/androidTest/java/ru/mirea/klishenkovai/notificationapp/ExampleInstrumentedTest.java
+++ notificationapp/src/androidTest/java/ru/mirea/klishenkovai/notificationapp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ru.mirea.klishenkovaai.notificationapp;
+package ru.mirea.klishenkovai.notificationapp;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.mirea.klishenkovaai.notificationapp", appContext.getPackageName());
+        assertEquals("ru.mirea.klishenkovai.notificationapp", appContext.getPackageName());
     }
 }
